267011455140f396772cfdb3cdd257518f12e5c9
Smiley <jcsmileyjr@gmail.com>
2020-12-12 18:44:09 -0600

    Feature: user can answer the problem to get points

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ export class AppComponent {
   score = 0;
   timer =60;
   gameOver = false;
+  answers = ["eyeball"];
+  userAnswer = " ";
+
   constructor(){
     setInterval(()=> this.countdown(), 1000);
   }
@@ -19,6 +22,15 @@ export class AppComponent {
     this.timer = this.timer - 1;
     if(this.timer <= 0){
       this.gameOver = true;
+    }
+  }
+
+  /**TODO: 
+   * 1. Strip userAnswer of any blank spaces to avoid errors.
+   */
+  submitAnswer(){
+    if((this.userAnswer).trim() === this.answers[0]){
+      this.score += 100;
     }
   }
 
